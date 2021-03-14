292ae690595d1a1a5bd24ff1da800a54b398c49b
jean sanon <jeansanon180@gmail.com>
2021-03-14 18:03:59 -0400

    Day2 activities

---
+++ resources/day2/1_day2.py
@@ -0,0 +1,18 @@
+# This program uses string concactenation to ask user for his/her name to sing Happy Birthday
+# ----- Sing Happy Birthday -----
+
+print()
+
+user = input('Enter your name: ').title()
+
+#name = 'Arianne'
+
+print('Happy birthday to you')
+
+print('Happy birthday to you')
+
+print('Happy birthday dear '+ user + '!')
+
+print('Happy birthday to you')
+
+print()
