0e4d7baea322da592c13381997479177bc8d07c4
jean sanon <jeansanon180@gmail.com>
2021-03-16 20:55:32 -0400

    dice simulator

---
+++ resources/day3/dice_simulator.py
@@ -0,0 +1,15 @@
+"""
+Randomly returns 2 numbers from 1 to 6
+"""
+# importing library named 'random'
+import random
+
+# Generating 2 random numbers between 1-6 (inclusively)
+
+number1 = random.randint(1, 6)
+
+number2 = random.randint(1, 6)
+
+print('\nThe result of 2 dices was: ' + str(number1) + ' and ' + str(number2))
+
+print()
